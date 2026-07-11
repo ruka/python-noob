a6ea2711c704794f884b561bfae7290611c87602
ruka <1305273024@qq.com>
2026-07-11 15:11:00 +0800

    新添加侧边栏，流式输出，客户端对ai的昵称和个性的修改

--- ai应用/ai伴侣.py
+++ ai应用/ai伴侣.py
@@ -27,6 +27,14 @@ for message in st.session_state.message:
 # logo
 st.logo("https://oss.5eplay.com/cloudx/p/sport/csgo/team/merge_d818n3cconkpudhr4jmg.png?x-oss-process=image/indexcrop,x_300,i_1/resize,h_150,m_lfit")
 
+# 侧边栏设置
+with st.sidebar:
+    st.sidebar.title("AI智能伴侣您的智能好伙伴")
+    ai_name = st.text_input("昵称")
+    ai_nature = st.text_area("个性/人设")
+
+text = f"你的名字叫{ai_name},{ai_nature}"
+
 # 调用deepseek大模型
 client = OpenAI(
     api_key=os.environ.get('DEEPSEEK_API_KEY'),
@@ -44,17 +52,29 @@ if prompt:
     response = client.chat.completions.create(
         model="deepseek-v4-pro",
         messages=[
-            {"role": "system", "content": "你是一个精通计算机的专家，你的名字叫kk"},  # 智能体的人设
-            {"role": "user", "content": prompt},  # 用户提示词
+            {"role": "system", "content": text},  # 智能体的人设
+            # {"role": "user", "content": prompt},  # 用户提示词
+            *st.session_state.message  # 解包所存储的所有信息
         ],
-        stream=False,
+        stream=True,
         reasoning_effort="high",
         extra_body={"thinking": {"type": "enabled"}}
     )
 
-    # 输出大模型返回的内容
-    print(f"大模型输出的是：{response.choices[0].message.content}")
-    st.chat_message("assistant").write(response.choices[0].message.content)
+    # 输出大模型返回的内容(输出格式为非流式方式)
+    # print(f"大模型输出的是：{response.choices[0].message.content}")
+    # st.chat_message("assistant").write(response.choices[0].message.content)
+
+    # 输出大模型返回的内容(输出格式为流式方式)
+    full_content = ""
+    with st.chat_message("assistant"):
+        placeholder = st.empty()
+        for chunk in response:
+            if chunk.choices[0].delta.content is not None:
+                content = chunk.choices[0].delta.content
+                full_content += content
+                placeholder.markdown(full_content)
+
     # 保存大模型返回的内容
     st.session_state.message.append(
-        {"role": "assistant", "content": response.choices[0].message.content})
+        {"role": "assistant", "content": full_content})
